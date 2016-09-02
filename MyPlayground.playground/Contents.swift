1f12a236f8770cbc9968957f9b75b63130747e2a
Dougly <dgalante11@gmail.com>
2016-09-02 12:00:30 -0400

    playing with tuples

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let person = (name: "Doug", age: 28)
+
+print (person.name)
+print (person.age)
+
+print ("\(person.name) is \(person.age) years old")
+
+//person.age = 22
+
+var person2 = (name: "Matt", age: 25, height: "6\'")
+person2.height = "5' 10\""
+
+print (person2)
+print (person2.height)
+
